9b2735dbd60e395c14d0014d7dd6984f5e55b2c8
Abdul Raheem <abdul.raheem.30140@gmail.com>
2023-10-25 19:46:36 +0500

    Update config.js

--- src/api/config.js
+++ src/api/config.js
@@ -1,4 +1,4 @@
 export const BASE_URL =
-  process.env.REACT_APP_NODE_ENV !== 'local'
+  process.env.REACT_APP_NODE_ENV === 'local'
     ? 'http://localhost:5000/api/v1'
     : 'https://ams-api.vercel.app/api/v1';
